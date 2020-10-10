9752a8ac4838711ac77de855b91db3e0710a2a7a
Sheweta <sheweta@gmail.com>
2020-10-10 17:32:23 +0000

    add list_view

--- employee/views.py
+++ employee/views.py
@@ -9,11 +9,18 @@ from django.http import HttpResponse
 def PersonView(request): 
     # dictionary for initial data with  
     # field names as keys 
-    context={}    
-    # add the dictionary during initialization 
-    form = PersonForm(request.POST or None) 
-    if form.is_valid(): 
-        form.save() 
-        return HttpResponse("Person is added in the system") 
-    context['form']= form 
-    return render(request, "employee/person.html", context) 
+    context={}  
+    if request.method == 'GET':
+        context["dataset"] = Person.objects.all() 
+        return  render(request, "employee/person_list.html", context)
+    elif request.method == 'POST':
+         
+        # add the dictionary during initialization 
+        form = PersonForm(request.POST or None) 
+        if form.is_valid(): 
+            form.save() 
+            context['message']="Person Details Added to  the system"
+            context["dataset"] = Person.objects.all() 
+            return  render(request, "employee/person_list.html", context)
+        context['form']= form 
+        return render(request, "employee/person.html", context) 
